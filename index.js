231c9968416af0c7fc8838dc9ed1e544c73e7c68
Ignacio R Espino <irespino@capozios.com>
2018-08-01 14:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
-name = "Susan"
-height = 39;
-module.exports = {name, height, message}
+var name = "Susan";
+var height = 39;
+var message = ` is ${height} inches tall`;
+module.exports = {name, height, message};
